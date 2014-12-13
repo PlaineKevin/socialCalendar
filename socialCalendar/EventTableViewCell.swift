f3b5ade8ea9b9e8e8ce6b2c6f34f4d824c8f0670
Miles Crabill <miles@milescrabill.com>
2014-12-13 13:40:52 +0100

    added events classes

---
+++ socialCalendar/EventTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventTableViewCell.swift
+//  socialCalendar
+//
+//  Created by Miles Crabill on 12/12/14.
+//  Copyright (c) 2014 AIT. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
